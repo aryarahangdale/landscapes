9bc3e072b187d9bac7b87eba7cd5b93170225f50
aryarahangdale <aryarahangdale25@gmail.com>
2026-01-11 12:02:35 +0530

    Initial commit - Landscaping website

---
+++ src/main/java/com/pragati/landscapes/LandscapesApplication.java
@@ -0,0 +1,13 @@
+package com.pragati.landscapes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LandscapesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LandscapesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pragati/landscapes/LandscapesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pragati.landscapes;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LandscapesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
